aa92317a3d0e7d6dc26b5cc11cec6372e164b143
Chandler Scott <scottcd1@etsu.edu>
2023-05-12 16:23:06 -0400

    moving this to python

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ ! -d "build" ]
-then
-    mkdir build
-fi
-cd build
-cmake -DCMAKE_PREFIX_PATH=/usr/local/lib/libtorch -D CMAKE_C_COMPILER=gcc -D CMAKE_CXX_COMPILER=g++-11 ..
-cmake --build . --config Release
